578bda793c1e97aedb79028d5eda7ba7dc4ee53e
Cesar <hscesar@gmail.com>
2022-04-22 16:18:30 -0400

    App test updated

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const paragraph = screen.getByText(/Hello CMGP/i);
+  expect(paragraph).toBeInTheDocument();
 });
